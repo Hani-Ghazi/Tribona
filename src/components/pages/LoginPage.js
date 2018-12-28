88e974f04a9b4f9c0758cf83994b8762435faf8b
Hani Ghazi <hani1993ghazi@gmail.com>
2018-12-28 16:42:35 +0200

    Set up the Route with couples of files

---
+++ src/components/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import LoginForm from '../forms/LoginForm.js';
+
+const LoginPage = () => (
+  <div>
+    <h1>Login Page</h1>
+    <LoginForm></LoginForm>
+  </div>
+);
+
+export default LoginPage;
