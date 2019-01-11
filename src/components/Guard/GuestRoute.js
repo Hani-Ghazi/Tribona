0933df9b0a67a2ee4c112a6803b64dfe86ccc474
Hani Ghazi <hani1993ghazi@gmail.com>
2019-01-11 20:27:05 +0200

    [Fix]: Guard route rule.

--- src/components/Guard/GuestRoute.js
+++ src/components/Guard/GuestRoute.js
@@ -4,9 +4,7 @@ import { Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 const GuestRoute = ({ isAuthenticated, component: Component, ...rest }) => (
-  <Route {...rest} render={props =>
-    isAuthenticated ? <Component {...props}/> : <Redirect to={"/"}/>
-  }/>
+  <Route {...rest} render={props => <Component {...props}/>}/>
 );
 
 GuestRoute.propTypes = {
