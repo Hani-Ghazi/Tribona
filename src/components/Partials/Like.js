d1f29950cb4c2c52bb3bf8b57ee509763718ef31
Hani Ghazi <hani1993ghazi@gmail.com>
2019-01-18 15:17:00 +0200

    [Add]: Like component.

---
+++ src/components/Partials/Like.js
@@ -0,0 +1,8 @@
+import React from "react";
+import {IoIosThumbsUp } from "react-icons/io";
+
+const Like = ({ isLike, onChange }) => {
+  return <IoIosThumbsUp size="2em" className="pointer" onClick={onChange} color={isLike && "#ff4f81"}/>;
+};
+
+export default Like;
