3486f5cd3c8cb119a7da6942151dbd4c1b729f74
Hani Ghazi <hani1993ghazi@gmail.com>
2019-01-28 02:16:31 +0200

    [Remove]: users from navbar.

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -28,9 +28,6 @@ const Header = ({ isAuthenticated, logout, user }) => (
           <li className="nav-item">
             <Link to={"/trips"} className={"nav-link  mr-3 open my-lg-0 my-2 ml-lg-0 ml-3"}>Trips</Link>
           </li>
-          <li className="nav-item">
-            <Link to={"/users"} className={"nav-link  mr-3 open my-lg-0 my-2 ml-lg-0 ml-3"}>Users</Link>
-          </li>
           {
             !isAuthenticated ?
               <li className="nav-item">
