b27b1769570add1eb0d7ac952e4fc52a1c3c11be
Hani Ghazi <hani1993ghazi@gmail.com>
2019-02-15 00:16:33 +0200

    [Add]: setup the api functions.

--- src/api/trips.js
+++ src/api/trips.js
@@ -1,5 +1,23 @@
-import api from './index';
+import api from "./index";
 
 export default {
-  getTrips: () => api.get('trips')
+  getTrips: (params) => api.get(`trips`, params),
+  getPopularTrips: () => api.get(`trips`),
+  getTripById: (id) => api.get(`trips/${id}`),
+  getTripComments: (id) => api(`trips/${id}/comments`),
+  tripLike: (id) => api.put(`trips/${id}/like`),
+  tripDisLike: (id) => api.delete(`trips/${id}/like`),
+  tripFav: (id) => api.put(`trips/${id}/favorite`),
+  tripUnFav: (id) => api.delete(`trips/${id}/favorite`),
+  addOrUpdateComment: ({ id, text, commentId }) => api.post(`trips/${id}/comments${!!commentId ? "/" + commentId : ""}`, { text }),
+  deleteComment: ({ id, commentId }) => api.delete(`trips/${id}/comments/${commentId}`),
+  getTripSteps: (id) => api.get(`trips/${id}/steps`),
+  createTrip: (trip) => api.post(`trips`, trip),
+  updateTrip: (trip) => api.put(`trips/${trip.id}`, trip),
+  createStep: (step) => api.post(`trips/${step.tripId}/steps`, step),
+  updateStep: (step) => api.put(`trips/steps/${step.id}`, step),
+  deleteStep: (id) => api.delete(`trips/steps/${id}`),
+  stepDisLike: (id) => api.delete(`trips/steps/${id}/like`),
+  stepLike: (id) => api.put(`trips/steps/${id}/like`),
+  rateTrip: (id, newRating) => api.post(`trips/${id}/rating`, { value: newRating })
 };
