087acbb13dbd1fe9e0909c4b7e345e8226f42e5e
Hani Ghazi <hani1993ghazi@gmail.com>
2019-02-17 11:03:46 +0200

    [Add]: missing files.

---
+++ src/components/Trips/TripForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class TripForm extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default TripForm;
